a3372008c628d9cff176f121904a51615fe98767
Lee Lunn <lee@torusoft.com>
2021-02-23 08:57:22 -0400

    Auto focus element

---
+++ app/modifiers/epm-on-insert-focus.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-productivity-modifiers/modifiers/epm-on-insert-focus';
---
+++ tests/integration/modifiers/epm-on-insert-focus-test.js
@@ -0,0 +1,15 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Modifier | epm-on-insert-focus', function(hooks) {
+  setupRenderingTest(hooks);
+
+  // Replace this with your real tests.
+  test('it renders', async function(assert) {
+    await render(hbs`<div {{epm-on-insert-focus}}></div>`);
+
+    assert.ok(true);
+  });
+});
